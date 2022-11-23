14fb18fda23d5208ae78d7060bd4491610ea0d26
MYUNGGWAN LEE <lmk960201@gmail.com>
2022-11-23 14:47:12 +0900

    Feat: add Sign(user table) in models

---
+++ models/Sign.js
@@ -0,0 +1,54 @@
+const { STRING, DOUBLE } = require("sequelize"); //어차피 비활성화 -> 없어도 된다.
+// const { ModifierFlags } = require('typescript');
+const Sign = function(Sequelize, DataTypes) {
+    const model = Sequelize.define(
+        'user', {
+            user_index: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                primaryKey: true,
+                autoIncrement: true,
+            },
+            user_id: {
+                type: DataTypes.STRING(15),
+                allowNull: false,
+                unique: true,
+            },
+            user_pw: {
+                type: DataTypes.STRING(15),
+                allowNull: false,
+            },
+            user_name: {
+                type: DataTypes.STRING(15),
+                allowNull: false,
+            },
+            user_email: {
+                type: DataTypes.STRING(35),
+                allowNull: false,
+                unique: true,
+            },
+            // user_adr: {
+            //     type: DataTypes.STRING(50),
+            //     allowNull: false,
+            // },
+            user_Lat: {
+                type: DataTypes.DOUBLE,
+                allowNull: true,
+            },
+            user_Lon: {
+                type: DataTypes.DOUBLE,
+                allowNull: true,
+            },
+            prof_img_url: {
+                type: DataTypes.TEXT,
+                allowNull: true,
+            }
+        }, {
+            tableName: 'user',
+            freezeTableName: true,
+            timestamps: false,
+        }
+    );
+    return model;
+}
+module.exports = Sign;
