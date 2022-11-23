eab06201cbf52eb663b7ed7002ed054a7a25f933
MYUNGGWAN LEE <lmk960201@gmail.com>
2022-11-23 13:38:29 +0900

    Feat: add routes files

--- controllers/Cboard.js
+++ controllers/Cboard.js
@@ -1,58 +1,68 @@
-const models = require('../models');
+const models = require('../models/index_board');
 
 exports.board = (req, res) => {
     res.render('board');
 };
 
 // GET /board : 게시글 전체 조회 
+// findAll() : 모든 데이터를 조회
+// findAll을 통해 얻어진 결과는 배열로 반환
+// 배열의 각 요소는 객체로 구성
+// 배열의 각 요소는 테이블의 한 행을 나타냄
 exports.getBoard = (req, res) => {
-    models.Board.findAll().then((result) => {
-        console.log('board controller: \nfindAll >> ' + result); // [ {}, {}, {}, {} ]
-        res.render('board', { data: result });
+    models.TBoard.findAll().then((result) => {
+        console.dir('board controller: findAll >> ', result); // [ {}, {}, {}, {} ]
+        // findAll() 결과를 routes/board.js에 전달
+        res.send(result);
     }).catch((err) => {
-        console.log('board controller: ' + err);
+        console.dir('board controller: ' + err);
     });
 };
 
 // GET /board/get : 게시글 하나 조회
-exports.getArticle = (req, res) => {
-    console.log('requested id: ' + req.params.id);
-    models.Board.findOne({
-        where: { board_id: req.query.board_id }
+exports.getArticleById = (req, res) => {
+    let id = req.params.id;
+    models.TBoard.findOne({
+        where: { id: id }
     }).then((result) => {
-        console.log('board controller: \nfindOne >> ' + result); // {}
+        console.dir('board controller: \nfindOne >> ', result); // { }
         res.send(result);
     }).catch((err) => {
-        console.log('board controller: ' + err);
+        console.dir('board controller: ' + err);
     });
 };
 
-// POST /board/write : 게시글 하나 추가
+// GET /board/write : 게시글 작성 페이지
+exports.writeArticle = (req, res) => {
+    res.render('write');
+};
+
+// POST /board/post : 게시글 하나 추가
 exports.postArticle = (req, res) => {
-    console.log('postArticle: ' + req.body);
-    models.Board.create({
+    console.dir('postArticle: ', req.body);
+    const data = req.body;
+    models.TBoard.create({
         title: data.title, // 폼에 입력한 title
         parity: data.parity, // 폼에서 선택한 parity
-        member_num: data.member_num, // 폼에 입력한 member_num
+        member_number: data.member_number, // 폼에 입력한 member_num
         description: data.description, // 폼에 입력한 description
-        created_dt: data.created_dt, // 게시물 생성일자
         expr_dt: data.expr_dt, // 게시물 만료일자
         start_dt: data.start_dt, // 모집 시작일자
         end_dt: data.end_dt, // 모집 종료일자
-        appo_time: data.appo_time, // 스터디 진행 약속 시간
-        appo_aria: data.appo_aria // 스터디 진행 약속 장소
+        // appo_time: data.appo_time, // 스터디 진행 약속 시간
+        // appo_aria: data.appo_aria // 스터디 진행 약속 장소
     }).then((result) => {
-        console.log('board controller: \ncreate >> ' + result); // {}
+        console.dir('board controller: \ncreate >> ', result); // {}
         res.send(result);
     }).catch((err) => {
-        console.log('board controller: ' + err);
+        console.dir('board controller: ', err);
     });
 };
 
 // PATCH /board/edit : 게시글 하나 수정
 exports.editArticle = (req, res) => {
-    console.log('editArticle: ' + req.body);
-    models.Board.update({
+    console.dir('editArticle: ', req.body);
+    models.TBoard.update({
         title: data.title, // 폼에 입력한 title
         parity: data.parity, // 폼에서 선택한 parity
         member_num: data.member_num, // 폼에 입력한 member_num
@@ -61,26 +71,41 @@ exports.editArticle = (req, res) => {
         expr_dt: data.expr_dt, // 게시물 만료일자
         start_dt: data.start_dt, // 모집 시작일자
         end_dt: data.end_dt, // 모집 종료일자
-        appo_time: data.appo_time, // 스터디 진행 약속 시간
-        appo_aria: data.appo_aria // 스터디 진행 약속 장소
+        // appo_time: data.appo_time, // 스터디 진행 약속 시간
+        // appo_aria: data.appo_aria // 스터디 진행 약속 장소
     }, {
-        where: { board_id: req.query.board_id }
+        where: { article_id: req.query.article_id }
     }).then((result) => {
-        console.log('board controller: \nupdate >> ' + result); // {}
+        console.dir('board controller: \nupdate >> ', result); // {}
         res.send('수정사항이 저장되었습니다.');
     }).catch((err) => {
-        console.log('board controller: ' + err);
+        console.dir('board controller: ', err);
     });
 };
 
+// GET /board/edit/:id : 게시글 수정 페이지
+exports.editArticlePage = (req, res) => {
+    let id = req.params.id;
+    models.TBoard.findOne({
+        where: { id: id }
+    }).then((result) => {
+        console.dir('board controller: \nfindOne >> ', result); // { }
+        res.render('edit', { data: result });
+    }).catch((err) => {
+        console.dir('board controller: ' + err);
+    });
+};
+
+// GET /board/search : 게시글 검색
+
 // DELETE /board/delete : 게시글 하나 삭제
 exports.deleteArticle = (req, res) => {
-    models.Board.destroy({
-        where: { board_id: req.query.board_id }
+    models.TBoard.destroy({
+        where: { article_id: req.query.article_id }
     }).then((result) => {
-        console.log('board controller: \ndestroy >> ' + result); // {}
+        console.dir('board controller: \ndestroy >> ' + result); // {}
         res.send('게시글이 삭제되었습니다.');
     }).catch((err) => {
-        console.log('board controller: ' + err);
+        console.dir('board controller: ', err);
     });
 };
---
+++ routes/board.js
@@ -0,0 +1,58 @@
+// TODO: 라우트 설정
+const express = require('express');
+const controllerB = require('../../study_group/controllers/CBoard');
+const controllerM = require('../../study_group/controllers/Cmain');
+const router = express.Router();
+
+// GET / => localhost:PORT/
+router.get('/', controllerM.main);
+
+// *** 스터디 모집글 게시판 관련 기능 ***
+
+// GET /board => localhost:PORT/board
+router.get('/board', async(req, res) => {
+    // controllers/Cboard에서 sequelize.findAll()로 반환된 데이터를 board.ejs에 전달
+    const result = await controllerB.getBoard(req, res);
+    res.render('board', { result });
+}); // 전체 조회
+
+// GET /board/get + id => localhost:PORT/board/get + id
+router.get('/board/get/id', function(req, res, next) {
+    let id = req.params.id;
+    controllerB.getArticleById(id, (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send
+        } else {
+            res.render('article', { data: result });
+        }
+    });
+}); // 하나 조회
+
+// GET /board/write => localhost:PORT/board/write
+router.get('/board/write', controllerB.writeArticle); // 게시글 작성 페이지: done
+
+// POST /board/write => localhost:PORT/board/write
+router.post('/board/post', controllerB.postArticle); // 하나 추가: done
+
+// GET /board/edit => localhost:PORT/board/edit
+router.get('/board/edit/id', function(req, res, next) {
+    let id = req.params.id;
+    controllerB.editArticlePage(id, (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send
+        } else {
+            res.render('edit', { data: result });
+        }
+    });
+}); // 하나 수정
+
+// PATCH /board/edit => localhost:PORT/board/edit
+router.patch('/board/edit', controllerB.editArticle); // 하나 수정
+
+// DELETE /board/delete => localhost:PORT/board/delete
+router.delete('/board/delete', controllerB.deleteArticle); // 하나 삭제
+
+
+module.exports = router;
