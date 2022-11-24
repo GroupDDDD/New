3510fd6c224880a6c1bc619c8ddabc00235691e2
SEUNGWON98 <98tjtmddnjs@Naver.com>
2022-11-24 13:51:17 +0900

    Start Page upload

--- app.js
+++ app.js
@@ -1,63 +1,46 @@
-const express = require('express');
-const path = require('path');
-const morgan = require('morgan');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+// const path = require('path');
+const session = require("express-session");
+const dotenv = require("dotenv");
+dotenv.config();
+const passport = require("passport");
+const PORT = 8500;
 
-// router
-const { sequelize } = require('./models/index_board'); // 시퀄라이즈
-const indexRouter = require('./routes/index');
-const boardRouter = require('./routes/board');
-
-// app
-const app = express();
-// port number setting
-app.set('port', process.env.PORT || 8500);
-// view engine setting
-app.set('view engine', 'ejs');
-// static file setting
-app.use('/views', express.static(__dirname + '/views'));
-app.use('/static', express.static(__dirname + '/static'));
-app.use('/static/css', express.static(__dirname + '/static/css'));
-app.use('/static/js', express.static(__dirname + '/static/js'));
-app.use('/static/img', express.static(__dirname + '/static/img'));
-
-// 서버 실행시 MYSQL과 연결
-sequelize.sync({ force: false }) // 서버 실행시마다 테이블을 재생성할건지에 대한 여부
-    .then(() => {
-        console.log('데이터베이스 연결 성공');
-    })
-    .catch((err) => {
-        console.error(err);
-    });
-
-app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+//라우터 분리
+const indexRouter = require("./routes/index");
+const signRouter = require("./routes/sign"); //localhost:8000/sign
+const { sequelize } = require("./models");
+const passportConfig = require("./passport");
 
 const app = express();
 passportConfig(); // 패스포트 설정
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
+app.use(morgan("dev"));
+app.use("/views", express.static(__dirname + "/views"));
+app.use("/static", express.static(__dirname + "/static"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-// router setting
 app.use("/", indexRouter);
-app.use("/board", boardRouter);
+app.use("/sign", signRouter);
 
-app.use((req, res, next) => {
-    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
-    error.status = 404;
-    next(error);
-});
-
-app.use((err, req, res, next) => {
-    res.locals.message = err.message;
-    res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
-    res.status(err.status || 500);
-    res.render('error');
-});
-
-app.listen(app.get('port'), () => {
-    let p = app.get('port');
-    console.log(p, '번 포트에서 대기 중');
-    console.log('~~~~~~~~~~~~~~~~~~~~~');
-    console.log(`http://localhost:${p}`);
-    console.log('~~~~~~~~~~~~~~~~~~~~~');
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
 });
--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,14 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/Cmain");
 
-router.get("/", controller.main);
+router.get("/", controller.start);
+
+router.get("/main", controller.main);
 
 //김예나 메인 코드
-router.get('/main2', controller.main2);
+router.get("/main2", controller.main2);
 
 router.get("/index", controller.index);
-
 
 router.get("/study", controller.study); // 전체 조회
 
@@ -17,5 +18,6 @@ router.get("/login", controller.login);
 
 router.get("/chat", controller.chat);
 
-module.exports = router;
+router.get("/front-board", controller.frontBoard);
 
+module.exports = router;
