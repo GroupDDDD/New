5503b7f39cb28d9336cf79994f971d1f7b3ba524
WooriSunoo <cherry21004@gmail.com>
2022-11-25 19:03:32 +0900

    part_id조회 에러 수정

--- controllers/Cpart.js
+++ controllers/Cpart.js
@@ -1,31 +1,31 @@
 const models = require("../models/index"); // ../models/index.js
 
 // part_id가져오기
-exports.getPartid = (req, res) => {
-    console.log(req.query);
-    // SELECT *FROM chat_participants WHERE room_id=${req.query.roomId}
-    // and user_index =${req.query.id}
-    models.Sign.findOne({
-        where: {
-            roomId: req.query.roomId,
-            id: req.query.id,
-        },
-    }).then((result) => {
-        console.log("findOne >> ", result);
-        res.send(result);
-    });
+exports.getpartid = (req, res) => {
+  console.log(req.query);
+  // SELECT *FROM chat_participants WHERE room_id=${req.query.roomId}
+  // and user_index =${req.query.id}
+  models.Part.findOne({
+    where: {
+      room_id: req.query.roomId,
+      user_index: req.query.id,
+    },
+  }).then((result) => {
+    console.log("findOne >> ", result);
+    res.send(result);
+  });
 };
 
 // 참가자테이블값생성(chat_participants)
 // INSERT INTO chat_participants (room_id, user_index, pub_status, createdAt) VALUES ('1', '1', '1', '20221122');
 
 exports.postPart = (req, res) => {
-    models.Part.create({
-        roomId: req.body.roomId,
-        userIndex: req.body.userIndex,
-        pubStatus: req.body.pubStatus,
-    }).then((result) => {
-        console.log("postPart create >> ", result);
-        res.send(result);
-    });
+  models.Part.create({
+    room_id: req.body.roomId,
+    user_index: req.body.userIndex,
+    pub_status: req.body.pubStatus,
+  }).then((result) => {
+    console.log("postPart create >> ", result);
+    res.send(result);
+  });
 };
