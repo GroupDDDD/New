b46673b4e9ec17e3357a3d6f8b3d544a8a68704a
kimyena1123 <kyq104@gmail.com>
2022-11-25 10:39:11 +0900

    fix: Cmain.js 코드 수정

--- controllers/Cmain.js
+++ controllers/Cmain.js
@@ -1,10 +1,14 @@
 // TODO: 컨트롤러 코드
 // Path: study_group/controllers/Cmain.js
 
+exports.start = (req, res) => {
+  res.render("start");
+};
+
 exports.main = (req, res) => {
-    res.render("main");
-  };
-  
+  res.render("main");
+};
+
 exports.main2 = (req, res) => {
   const user = req.session.user;
   console.log('req.session.user >> ', user);
@@ -19,21 +23,28 @@ exports.main2 = (req, res) => {
   }
     // res.render('main2');
   }
-  
-  exports.index = (req, res) => {
-    res.render('index');
-}
 
-  exports.study = (req, res) => {
-    res.render("study");
-  };
-  
-  exports.login = (req, res) => {
-    res.render("login");
-  };
-  
-  exports.chat = (req, res) => {
-    res.render("chat");
-  };
-  
+exports.index = (req, res) => {
+  res.render("index");
+};
 
+exports.study = (req, res) => {
+  res.render("study");
+};
+
+exports.login = (req, res) => {
+  res.render("login");
+};
+
+exports.chat = (req, res) => {
+  res.render("chat");
+};
+
+exports.frontBoard = (req, res) => {
+  res.render("front-board");
+};
+
+exports.mypage = (Req, res) => {
+  res.render("mypage");
+};
+  
