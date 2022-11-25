526012ae011e2b973e82d764cc4e4481905a1322
kimyena1123 <kyq104@gmail.com>
2022-11-25 17:31:34 +0900

    fix: app.js 수정

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 // sequelize-cli 자동 생성한 파일
 "use strict";
-​
+
 const Sequelize = require("sequelize");
 const config = require(__dirname + "/../config/config.json")["development"];
 // const a = require(__dirname + '/../config/config.json');
@@ -11,7 +11,7 @@ const config = require(__dirname + "/../config/config.json")["development"];
 // }
 // const config = a["development"];
 const db = {};
-​
+
 let sequelize = new Sequelize(
     config.database,
     config.username,
@@ -19,12 +19,12 @@ let sequelize = new Sequelize(
     config
 );
 // sequelize 객체 선언시 매개변수로 정보들을 받음
-​
+
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 // db = { sequelize: sequelize, Sequelize: Sequelize }
 // 잠깐주석처리!!!
-db.Sign = require("./Sign")(sequelize, Sequelize);
+db.Sign = require('./Sign')(sequelize, Sequelize);
 db.Board = require("./Board")(sequelize, Sequelize);
 db.Category = require("./Category")(sequelize, Sequelize);
 db.SCategory = require("./SCategory")(sequelize, Sequelize);
@@ -33,7 +33,7 @@ db.Chatcont = require("./Chatcont")(sequelize, Sequelize);
 db.Part = require("./Part")(sequelize, Sequelize);
 // models/Visitor.js 정의한 model이 db.Visitor에 들어감
 // db = { sequelize: sequelize, Sequelize: Sequelize, Visitor: model }
-​
+
 // Sign : Board = 1 : N
 db.Sign.hasMany(db.Board, {
     foreignKey: "user_id",
@@ -43,7 +43,7 @@ db.Board.belongsTo(db.Sign, {
     foreignKey: "user_id",
     targetKey: "user_index",
 });
-​
+
 // Board : SCategory = 1 : N
 db.Board.hasMany(db.SCategory, {
     foreignKey: "article_id",
@@ -51,7 +51,7 @@ db.Board.hasMany(db.SCategory, {
 db.SCategory.belongsTo(db.Board, {
     foreignKey: "article_id",
 });
-​
+
 // Category : SCategory = 1 : N
 db.Category.hasMany(db.SCategory, {
     foreignKey: "category_id",
@@ -59,7 +59,7 @@ db.Category.hasMany(db.SCategory, {
 db.SCategory.belongsTo(db.Category, {
     foreignKey: "category_id",
 });
-​
+
 // Board : Chat -> 1:N
 db.Board.hasMany(db.Chat, {
     foreignKey: "article_id",
@@ -67,7 +67,7 @@ db.Board.hasMany(db.Chat, {
 db.Chat.belongsTo(db.Board, {
     foreignKey: "article_id",
 });
-​
+
 // Chat : Part -> 1:N
 db.Chat.hasMany(db.Part, {
     foreignKey: "room_id",
@@ -75,7 +75,7 @@ db.Chat.hasMany(db.Part, {
 db.Part.belongsTo(db.Chat, {
     foreignKey: "room_id",
 });
-​
+
 // Part : Chatcont -> 1:N
 db.Part.hasMany(db.Chatcont, {
     foreignKey: "part_id",
@@ -83,7 +83,7 @@ db.Part.hasMany(db.Chatcont, {
 db.Chatcont.belongsTo(db.Part, {
     foreignKey: "part_id",
 });
-​
+
 // User : Part -> 1:N
 db.Sign.hasMany(db.Part, {
     foreignKey: "user_index",
@@ -91,7 +91,7 @@ db.Sign.hasMany(db.Part, {
 db.Part.belongsTo(db.Sign, {
     foreignKey: "user_index",
 });
-​
+
 // Part : Chatcont -> 1:N
 db.Part.hasMany(db.Chatcont, {
     foreignKey: "part_id",
@@ -99,6 +99,6 @@ db.Part.hasMany(db.Chatcont, {
 db.Chatcont.belongsTo(db.Part, {
     foreignKey: "part_id",
 });
-​
+
 module.exports = db;
 // db 변수 내보냄 -> 다른 파일에서 사용하기 때문
