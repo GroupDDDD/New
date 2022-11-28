09f2a35562077ea97c58cc4d61ae2bba2244452a
MYUNGGWAN LEE <lmk960201@gmail.com>
2022-11-28 16:27:04 +0900

    Chore: match local and rep

--- static/js/study.js
+++ static/js/study.js
@@ -4,33 +4,32 @@ const study = document.querySelector(".study");
 const study2 = document.querySelector(".study2");
 
 const cards = document.querySelectorAll(".card");
-const modals = document.querySelectorAll(".window-modal");
 
 for (const title of titles) {
-  title.addEventListener("click", function () {
-    for (let i = 0; i < titles.length; i++) {
-      titles[i].classList.remove("title-opacity");
-    }
-    title.classList.toggle("title-opacity");
-  });
+    title.addEventListener("click", function() {
+        for (let i = 0; i < titles.length; i++) {
+            titles[i].classList.remove("title-opacity");
+        }
+        title.classList.toggle("title-opacity");
+    });
 }
 
 // for(const title of titles) {
 //   title.addEventListener("click", )
 // }
 
-for (const card of cards) {
-  card.addEventListener("click", function () {
-    for (const modal of modals) {
-      modal.classList.toggle("visible");
-    }
-  });
-  modal.addEventListener("click", function () {
-    for (const modal of modals) {
-      modal.classList.toggle("visible");
-    }
-  });
-}
+// for (const card of cards) {
+//   card.addEventListener("click", function () {
+//     for (const modal of modals) {
+//       modal.classList.toggle("visible");
+//     }
+//   });
+//   modal.addEventListener("click", function () {
+//     for (const modal of modals) {
+//       modal.classList.toggle("visible");
+//     }
+//   });
+// }
 
 // for (const card of cards) {
 //   card.addEventListener("click", function () {
